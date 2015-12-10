7839bd8517ac34cdaa06c43fcfbfc8b6d80a9626
anovikov <anovikov@aif.ru>
2015-12-11 00:46:38 +0300

    10/12/2015 first commit

--- index.php
+++ index.php
@@ -15,7 +15,8 @@ namespace Application;
  * @param array $options
  * @return string
  */
-function get($pattern, array $options = array()){
+function get($pattern, array $options = array())
+{
     $path = '../view/'.$pattern;
     extract($options); ob_start(); @require($path);
     return ob_get_clean();
@@ -29,29 +30,37 @@ function get($pattern, array $options = array()){
 function __getAllHeaders($params)
 {
     $headers = array();
-    foreach ($_SERVER as $name => $value) {
-        if ((substr($name, 0, 5) == 'HTTP_') || ($name == 'CONTENT_TYPE') || ($name == 'CONTENT_LENGTH')) {
+    foreach ($_SERVER as $name => $value)
+        if ((substr($name, 0, 5) == 'HTTP_') || ($name == 'CONTENT_TYPE') || ($name == 'CONTENT_LENGTH'))
             $headers[str_replace(array(' ', 'Http'), array('-', 'HTTP'), ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
-        }
-    }
     return $headers;
 }
 
+$config = require('../config.php');
 $route = array_filter(explode("/", substr(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),1)));
 $header = (function_exists('getallheaders')) ? getallheaders() : __getAllHeaders($_SERVER);
 $params = array();
-switch ($_SERVER['REQUEST_METHOD']) {
+switch ($_SERVER['REQUEST_METHOD'])
+{
     case 'PUT':
     case 'POST':
         parse_str(file_get_contents('php://input'), $params);
-        $pattern = (($name = key($params)) ? $name . ".phtml" : "index.phtml");
+        $pattern = (($name = key($params)) ? $name . '.phtml' : 'index.phtml');
         break;
     case 'GET':
     case 'DELETE':
     default:
         parse_str(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), $params);
-        $pattern = (($name = end($route)) ? $name . ".phtml" : "index.phtml");
+        $pattern = (($name = current($route)) ? $name . '.phtml' : 'index.phtml');
 }
-echo get($pattern, array('params'=> $params, 'header' => $header, 'route'=> $route));
+echo get($pattern, array(
+    'params' => $params,
+    'header' => $header,
+    'route' => $route,
+    'config' => $config,
+    'json' => function (array $params){
+        echo json_encode($params);
+        exit(1);
+    }));
 exit(1);
 ?>
